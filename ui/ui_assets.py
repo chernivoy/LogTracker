4c91a34122973c1f3e4e5b13a99f1bffa01e36e7
ichernevoy <chernivoy@gmail.com>
2026-07-29 12:59:54 +0200

    update 01

--- ui/ui_assets.py
+++ ui/ui_assets.py
@@ -1,9 +1,24 @@
 # Файл: ui_assets.py
 
 import os
+import sys
+
+
+def resource_path(relative_path):
+    """ Отримує абсолютний шлях до ресурсів для PyInstaller та розробки """
+    try:
+        # PyInstaller створює тимчасову папку і зберігає шлях у _MEIPASS
+        base_path = sys._MEIPASS
+    except AttributeError:
+        # Якщо запущено просто як скрипт .py
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 
 # Шляхи до іконок
-HEADER_ICON_PATH = os.path.join("src", "Header.ico")
+# HEADER_ICON_PATH = os.path.join("src", "Header.ico")
+HEADER_ICON_PATH = resource_path(os.path.join("src", "Header.ico"))
 BUG_ICON_PATH = os.path.join("src", "bug2.png")
 CLOSE_ICON_PATH = os.path.join("src", "close.png")
 BURGER_MENU_ICON_PATH = os.path.join("src", "burger_menu.png")
